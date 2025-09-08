ef9ae0f88eeee4621a410b66fa41c8f8ffefa7e2
Andrew Porter <andrew.porter@stfc.ac.uk>
2025-09-08 14:20:11 +0100

    #468 fix Black formatting

--- src/fparser/common/tests/test_readfortran.py
+++ src/fparser/common/tests/test_readfortran.py
@@ -1735,7 +1735,7 @@ def test_conditional_include_omp_conditional_liness_free_format_multiple():
 def test_process_directives_option_read_fortran():
     """Test handling of the process_directives option.
 
-    Note that the funcionality tests for this option are in 
+    Note that the funcionality tests for this option are in
     fparser/two/tests/test_comments_and_directives.py"""
 
     input_text = "!$omp target\n! comment"
